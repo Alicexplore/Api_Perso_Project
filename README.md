df02bc122ebfffb99ab98ff3cc4727da3d8ba0b8
Alice <102388803+Alicexplore@users.noreply.github.com>
2022-12-19 16:49:35 +0100

    last

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # The_Picture_Project
 
-The Picture Project is a little perso project for those who
+The Picture Project is an App to find your destination by selecting photographies from the UNSPLASH Api.
+Lest's play !
 
-get the picture like blur xhen press on it to select
+
+
+
+IDEAS : get the picture like blur xhen press on it to select
--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// to do
+async function callSystemTrappist1 () {
+    try {
+        const response = await fetch(``);
+        let data = await response.json();
+        let promise = data.records
+        console.log("photographies from UNSPLASH", promise)
+        return promise;
+    }
+    catch (error){
+        console.log ("Message d'erreur: ", error.message);
+    }
+}
