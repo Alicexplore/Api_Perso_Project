38b8f04f1f383ffcd7b466f1554e060a49917c92
Alice <102388803+Alicexplore@users.noreply.github.com>
2022-12-20 17:25:34 +0100

    js

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-let clientID = "RXFOgUZNawven_5zeOt25eNeZf7BZ2KyJWk3JWyhr-Y";
-let endpoint = `https://api.unsplash.com/collections/4538662/photos/?client_id=${clientID}`;
-
-let imageElement = document.querySelector("#unsplashImage");
-let imageLink = document.querySelector("#imageLink");
-let creator = document.querySelector("#creator");
-
-fetch(endpoint)
-    .then(function(response) {
-    return response.json();
-   })
-    .then(function (jsonData) {
-    console.log(jsonData);
-    imageElement.src = jsonData.urls.regular;
-   });
-    
+// to do wednesday
    
