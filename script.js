9c510eb20a4052ce1e00b0de974f96fce322a8e2
Alice <102388803+Alicexplore@users.noreply.github.com>
2022-12-20 22:03:40 +0100

    js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ let arrayNepal = []
 let arrayBolivia = []
 let arrayUtah = []
 
+// lancement des premières photos
+const startTheGame = () => {
+
+}
+
+// évènements sur les clicks de sélection --> chargement des nouveaux duos de photos et suppression des photos passées
+const eventOnClick = () => {
+
+}
+
+// conditions de fin (remplissage des tableaux) et du calcul du pays gagnant
+const calculateWinnerCountry = () => {
+
+}
+
+// fin de jeu
+const endingTheGame = () => {
+
+}
+
