1ee1461688762fe758bb3abdd0f3c5ef2fce6a56
Alice <102388803+Alicexplore@users.noreply.github.com>
2022-12-20 13:03:45 +0100

    midi

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let clientID = "RXFOgUZNawven_5zeOt25eNeZf7BZ2KyJWk3JWyhr-Y";
-let endpoint = `https://api.unsplash.com/photos/?client_id=${clientID}`;
+let endpoint = `https://api.unsplash.com/collections/4538662/?client_id=${clientID}`;
 
 fetch(endpoint)
     .then(function(response) {
     return response.json();
    })
-    .then(function (jsonData){
 
-    })
     
+   
