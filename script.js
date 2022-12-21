b895df6e8c8b4eb2154c77ce60db4dbe6528bfaa
Alice <102388803+Alicexplore@users.noreply.github.com>
2022-12-21 15:26:53 +0100

    js

--- script.js
+++ script.js
@@ -1,57 +1,61 @@
-let arrayScoretNewzealand = []
-let arrayScoreIceland = []
-let arrayScoreHawaii = []
-let arrayScoreNepal = []
-let arrayScoreBolivia = []
-let arrayScoreUtah = []
+let photosNewzealand = ["","","",""]
+let photosIceland = ["","","",""]
+let photosHawaii = ["","","",""]
+let photosNepal = ["","","",""]
+let photosBolivia = ["","","",""]
+let photosUtah = ["","","",""]
 
-let photosNewzealand = ""; ""; ""; "";
-let photosIceland = ""; ""; ""; "";
-let photosHawaii = ""; ""; ""; "";
-let photosNepal = ""; ""; ""; "";
-let photosBolivia = ""; ""; ""; "";
-let photosUtah = ""; ""; ""; "";
+let scoreNewzealand = 0
+let scoreIceland = 0
+let scoreHawaii = 0
+let scoreNepal = 0
+let scoreBolivia = 0
+let scoreUtah = 0
 
-// lancement des premières photos
+// lancement des premières photos // nécessaire ?
 const startTheGame = () => {
 
 }
 
 // évènements sur les clicks de sélection --> chargement des nouveaux duos de photos et suppression des photos passées
 const eventOnClick = () => {
-    
+
+    if ( photosNewzealand.addEventListener === true) { //for all countries
+        scoreNewzealand++;
+    } 
 }
 
-// conditions de fin (remplissage des tableaux) et du calcul du pays gagnant
+// calcul du pays gagnant --> au dernier clic
 const calculateWinnerCountry = () => {
+
     for (let i = O; i <= arrayScoretNewzealand.length; i++){
         if ([i] > 3){
-            console.log("You should go to New Zealand")
+            console.log("You should go to New Zealand");
         } 
     }
     for (let i = O; i <= arrayScoreIceland.length; i++){
         if (arrayScoreIceland[i] >= 3){
-            console.log("You should go to Iceland")
+            console.log("You should go to Iceland");
         } 
     }
     for (let i = O; i <= arrayScoreHawaii.length; i++){
         if (arrayScoreHawaii[i] >= 3){
-            console.log("You should go to Hawaii")
+            console.log("You should go to Hawaii");
         } 
     }
     for (let i = O; i <= arrayScoreNepal.length; i++){
         if (arrayScoreNepal[i] >= 3){
-            console.log("You should go to Nepal")
+            console.log("You should go to Nepal");
         } 
     }
     for (let i = O; i <= arrayScoreBolivia.length; i++){
         if (arrayScoreBolivia[i] >= 3){
-            console.log("You should go to Bolivia")
+            console.log("You should go to Bolivia");
         } 
     }
     for (let i = O; i <= arrayScoreUtah.length; i++){
         if (arrayScoreUtah[i] >= 3){
-            console.log("You should go to Utah")
+            console.log("You should go to Utah");
         } 
     }
     
