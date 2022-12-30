99abf86718a5103bc53ad9a38477a31f96cf1dc3
Alice <102388803+Alicexplore@users.noreply.github.com>
2022-12-30 23:10:12 +0100

    popup window ok

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ let scoreNepal = 0;
 let scoreBolivia = 0;
 let scoreUtah = 0;
 
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+      },
+      500
+  )
+});
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+});
+
 function changeImage(image) {
     image.src = './italy/italy2.jpg';
 }
