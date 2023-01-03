d58ef85832c69822ccdff0192c2c7e9661c2f183
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-01-03 12:18:52 +0100

    hell

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ let titleRight = document.getElementById("titleRight1");
 titleRight.addEventListener("click", () => {
     alert("Hello Test Right");
     scoreItaly++; 
-    image2.src = './hawaii/hawaii2.jpg';  o
+    image2.src = './hawaii/hawaii2.jpg'; 
 });
     
 // imageLeft.addEventListener("mouseover","mouseleave" () => { // TOP TOP TOP
