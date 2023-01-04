bb9ca6156b7f891cc23195c5dd3b254137300311
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-01-04 20:48:23 +0100

    fixingdownloadphotos

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// window pop up
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+      },
+      500
+  )
+});
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+});
+
 let photosNewzealand = ["newzealand/newzealand1.jpg","newzealand/newzealand2.jpg","newzealand/newzealand3.jpg","newzealand/newzealand4.jpg"]
 let photosItaly = ["italy/italy1.jpg","italy/italy2.jpg","italy/italy3.jpg","italy/italy4.jpg"]
 let photosHawaii = ["hawaii/hawaii1.jpg","hawaii/hawaii2.jpg","hawaii/hawaii3.jpg","hawaii/hawaii4.jpg"]
@@ -44,30 +57,11 @@ let scoreNepal = 0
 let scoreBolivia = 0
 let scoreUtah = 0
 
-// window pop up
-window.addEventListener("load", function(){
-  setTimeout(
-      function open(event){
-          document.querySelector(".popup").style.display = "block";
-      },
-      500
-  )
-});
-document.querySelector("#close").addEventListener("click", function(){
-  document.querySelector(".popup").style.display = "none";
-});
-
 // change image little function trying
 
 function changeImage(image) {
     image.src = './italy/italy2.jpg';
 }
-
-// random photos left
-// const randomPhoto = (choosePhoto) => {
-//   let choosePhoto = photosNepal[Math.floor(Math.random() * photosNepal.length)] ; 
-    //  alert(choosePhoto);
-// }
 
 // click add score to country variable // change the photo
 
@@ -75,19 +69,15 @@ let titleLeft = document.getElementById("titleLeft1");
 titleLeft.addEventListener("click", () => {
     alert("Hello Test Left");
       scoreBolivia++; 
-      image.src = './nepal/nepal3.jpg';
+      imageOnLeft.src = './nepal/nepal4.jpg';
 });
 
 let titleRight = document.getElementById("titleRight1");
 titleRight.addEventListener("click", () => {
     alert("Hello Test Right");
     scoreItaly++; 
-    image.src = './hawaii/hawaii2.jpg'; 
+    imageOnRight.src = './utah/utah4.jpg'; 
 });
-    
-// imageLeft.addEventListener("mouseover","mouseleave" () => { // TOP TOP TOP
-//     alert("hello");
-// });
 
 // calcul du pays gagnant --> au dernier clic
 const calculateWinnerCountry = () => {
@@ -141,3 +131,18 @@ let winnerUtah = ""
 const endingTheGame = () => {
   
 }
+
+
+
+
+
+// random photos left
+// const randomPhoto = (choosePhoto) => {
+//   let choosePhoto = photosNepal[Math.floor(Math.random() * photosNepal.length)] ; 
+    //  alert(choosePhoto);
+// }
+
+   
+// imageLeft.addEventListener("mouseover","mouseleave" () => { // TOP TOP TOP
+//     alert("hello");
+// });
