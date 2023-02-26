4e0a8b19570067fd5d059421ddbb383f6d7c48f3
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-02-26 18:10:53 +0100

    last changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/////////////////////////////////////////////////////// MODAL WINDOW OPENING /////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////// MODAL WINDOW OPENING //////////////////////////////////////////////////////////////////////////
 
 window.addEventListener("load", function(){ // OPEN
   setTimeout(
@@ -12,287 +12,113 @@ window.addEventListener("load", function(){ // OPEN
   document.querySelector("#close").addEventListener("click", function(){ //CLOSE 
   document.querySelector(".popup").style.display = "none";
   document.querySelector(".backdrop").style.display = "none";
-});
+});  
 
-/////////////////////////////////////////////////////// SCORE VARIABLES /////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////// GAME //////////////////////////////////////////////////////////////////////////////////
 
-var scores = {
-  "Australia": 0,
-  "Bolivia": 0,
-  "Cuba": 0,
-  "Hawaii": 0,
-  "Iceland": 0,
-  "India": 0,
-  "Italy": 0,
-  "Japan": 0,
-  "Kenya": 0,
-  "Nepal": 0,
-  "Newzealand": 0,
-  "Utah": 0
-};
-
-/////////////////////////////////////////////////////// TRYING OBJECTS /////////////////////////////////////////////////////////////
-
-var photoList = {
-  "Australia": ["./australia/australia1.jpg", "./australia/australia2.jpg", "./australia/australia3.jpg", "./australia/australia4.jpg"],
-  "Bolivia": ["./bolivia/bolivia1.jpg", "./bolivia/bolivia2.jpg", "./bolivia/bolivia3.jpg", "./bolivia/bolivia4.jpg"],
-  "Cuba": ["./cuba/cuba1.jpg", "./cuba/cuba2.jpg", "./cuba/cuba3.jpg", "./cuba/cuba4.jpg"],
-  "Hawaii": ["./hawaii/hawaii1.jpg", "./hawaii/hawaii2.jpg", "./hawaii/hawaii3.jpg", "./hawaii/hawaii4.jpg"],
-  "Iceland": ["./iceland/iceland1.jpg", "./iceland/iceland2.jpg", "./iceland/iceland3.jpg", "./iceland/iceland4.jpg"],
-  "India": [ "./india/india1.jpg", "./india/india2.jpg", "./india/india3.jpg", "./india/india4.jpg"],
-  "Italy": ["./italy/italy1.jpg", "./italy/italy2.jpg", "./italy/italy3.jpg", "./italy/italy4.jpg"],
-  "Japan": [ "./japan/japan1.jpg", "./japan/japan2.jpg", "./japan/japan3.jpg", "./japan/japan4.jpg"],
-  "Kenya": ["./kenya/kenya1.jpg", "./kenya/kenya2.jpg", "./kenya/kenya3.jpg", "./kenya/kenya4.jpg"],
-  "Nepal": ["./nepal/nepal1.jpg", "./nepal/nepal2.jpg", "./nepal/nepal3.jpg", "./nepal/nepal.jpg"],
-  "Newzealand": ["./newzealand/newzealand1.jpg", "./newzealand/newzealand2.jpg", "./newzealand/newzealand3.jpg", "./newzealand/newzealand4.jpg"],
-  "Utah": ["./utah/utah1.jpg", "./utah/utah2.jpg", "./utah/utah3.jpg", "./utah/utah4.jpg"]
-};
-
-/////////////////////////////////////////////////////// ARRAY OF PICTURES LEFTSIDE /////////////////////////////////////////////////////////////
-
-var photoListLeft = [
-  "./newzealand/newzealand1.jpg",
-  "./newzealand/newzealand2.jpg",
-  "./newzealand/newzealand3.jpg",
-  "./newzealand/newzealand4.jpg",
-  "./italy/italy1.jpg",
-  "./italy/italy2.jpg",
-  "./italy/italy3.jpg",
-  "./italy/italy4.jpg",
-  "./hawaii/hawaii1.jpg",
-  "./hawaii/hawaii2.jpg",
-  "./hawaii/hawaii3.jpg",
-  "./hawaii/hawaii4.jpg",
-  "./nepal/nepal1.jpg",
-  "./nepal/nepal2.jpg",
-  "./nepal/nepal3.jpg",
-  "./nepal/nepal4.jpg",
-  "./bolivia/bolivia1.jpg",
-  "./bolivia/bolivia2.jpg",
-  "./bolivia/bolivia3.jpg",
-  "./bolivia/bolivia4.jpg",
-  "./utah/utah1.jpg",
-  "./utah/utah2.jpg",
-  "./utah/utah3.jpg",
-  "./utah/utah4.jpg"
-]
-
-/////////////////////////////////////////////////////// ARRAY OF PICTURES RIGHTSIDE /////////////////////////////////////////////////////////////
-
-var photoListRight = [
-  "./australia/australia1.jpg",
-  "./australia/australia2.jpg",
-  "./australia/australia3.jpg",
-  "./australia/australia4.jpg",
-  "./cuba/cuba1.jpg",
-  "./cuba/cuba2.jpg",
-  "./cuba/cuba3.jpg",
-  "./cuba/cuba4.jpg",
-  "./iceland/iceland1.jpg",
-  "./iceland/iceland2.jpg",
-  "./iceland/iceland3.jpg",
-  "./iceland/iceland4.jpg",
-  "./india/india1.jpg",
-  "./india/india2.jpg",
-  "./india/india3.jpg",
-  "./india/india4.jpg",
-  "./japan/japan1.jpg",
-  "./japan/japan2.jpg",
-  "./japan/japan3.jpg",
-  "./japan/japan4.jpg",
-  "./kenya/kenya1.jpg",
-  "./kenya/kenya2.jpg",
-  "./kenya/kenya3.jpg",
-  "./kenya/kenya4.jpg"
-]
-
-/////////////////////////////////////////////////////// CHANGING PICTURES /////////////////////////////////////////////////////////////
-
-
-const generateImages = () => {
-  let randomLeft = Math.floor(Math.random() * photoListLeft.length);
-  let randomRight = Math.floor(Math.random() * photoListRight.length);
-
-  document.getElementById("imageOnLeft").src = photoListLeft[randomLeft];
-  document.getElementById("imageOnRight").src = photoListRight[randomRight];
-
-};
-
-// function selectedPhotos() {
-
-//   let selectedPhotos = [];
+const images = [
+  { src: "./newzealand/newzealand1.jpg", country: "newzealand"},
+  { src: "./newzealand/newzealand2.jpg", country: "newzealand"},
+  { src: "./newzealand/newzealand3.jpg", country: "newzealand"},
+  { src: "./newzealand/newzealand4.jpg", country: "newzealand"},
+  { src: "./italy/italy1.jpg", country: "italy"},
+  { src: "./italy/italy2.jpg", country: "italy"},
+  { src: "./italy/italy3.jpg", country: "italy"},
+  { src: "./italy/italy4.jpg", country: "italy"},
+  { src: "./hawaii/hawaii1.jpg", country: "hawaii"},
+  { src: "./hawaii/hawaii2.jpg", country: "hawaii"},
+  { src: "./hawaii/hawaii3.jpg", country: "hawaii"},
+  { src: "./hawaii/hawaii4.jpg", country: "hawaii"},
+  { src: "./nepal/nepal1.jpg", country: "nepal"},
+  { src: "./nepal/nepal2.jpg", country: "nepal"},
+  { src: "./nepal/nepal3.jpg", country: "nepal"},
+  { src: "./nepal/nepal4.jpg", country: "nepal"},
+  { src: "./bolivia/bolivia1.jpg", country: "bolivia"},
+  { src: "./bolivia/bolivia2.jpg", country: "bolivia"},
+  { src: "./bolivia/bolivia3.jpg", country: "bolivia"},
+  { src: "./bolivia/bolivia4.jpg", country: "bolivia"},
+  { src: "./utah/utah1.jpg", country: "utah"},
+  { src: "./utah/utah2.jpg", country: "utah"},
+  { src: "./utah/utah3.jpg", country: "utah"},
+  { src: "./utah/utah4.jpg", country: "utah"},
+  { src: "./australia/australia1.jpg", country: "australia"},
+  { src: "./australia/australia2.jpg", country: "australia"},
+  { src: "./australia/australia3.jpg", country: "australia"},
+  { src: "./australia/australia4.jpg", country: "australia"},
+  { src: "./cuba/cuba1.jpg", country: "cuba"},
+  { src: "./cuba/cuba2.jpg", country: "cuba"},
+  { src: "./cuba/cuba3.jpg", country: "cuba"},
+  { src: "./cuba/cuba4.jpg", country: "cuba"},
+  { src: "./iceland/iceland1.jpg", country: "iceland"},
+  { src: "./iceland/iceland2.jpg", country: "iceland"},
+  { src: "./iceland/iceland3.jpg", country: "iceland"},
+  { src: "./iceland/iceland4.jpg", country: "iceland"},
+  { src: "./india/india1.jpg", country: "india"},
+  { src: "./india/india2.jpg", country: "india"},
+  { src: "./india/india3.jpg", country: "india"},
+  { src: "./india/india4.jpg", country: "india"},
+  { src: "./japan/japan1.jpg", country: "japan"},
+  { src: "./japan/japan2.jpg", country: "japan"},
+  { src: "./japan/japan3.jpg", country: "japan"},
+  { src: "./japan/japan4.jpg", country: "japan"},
+  { src: "./kenya/kenya1.jpg", country: "kenya"},
+  { src: "./kenya/kenya2.jpg", country: "kenya"},
+  { src: "./kenya/kenya3.jpg", country: "kenya"},
+  { src: "./kenya/kenya4.jpg", country: "kenya"}
+];  
   
-//   let selectedPhotoLeft = photoListLeft[randomLeft]; 
-//   selectedPhotos.push(selectedPhotoLeft);
-//   alert(`Selected photos: ${selectedPhotos}`);
+const trash = [];
+const points = {};
 
-//   let selectedPhotoRight = photoListRight[randomRight]; 
-//   selectedPhotos.push(selectedPhotoRight);
-//   alert(`Selected photos: ${selectedPhotos}`);
+function generateRandomImages() {
+  let randomIndex1 = Math.floor(Math.random() * images.length);
+  let randomIndex2 = Math.floor(Math.random() * images.length);
 
-// }
+  while (randomIndex2 === randomIndex1 || trash.includes(randomIndex1) || trash.includes(randomIndex2)) {
+    randomIndex1 = Math.floor(Math.random() * images.length);
+    randomIndex2 = Math.floor(Math.random() * images.length);
+  }
 
-const changePicturesLeftAndRight = () => {
+  const leftImg = document.getElementById("left-img");
+  const rightImg = document.getElementById("right-img");
 
-  let displayedPhotosLeft = [];
-  let displayedPhotosRight = [];
+  leftImg.src = images[randomIndex1].src;
+  rightImg.src = images[randomIndex2].src;
 
-  let imageLeft = document.getElementById("imageLeft");
-  let imageRight = document.getElementById("imageRight");
-  
-  // PHOTOS LEFT  
-
-  imageLeft.addEventListener("click", () => {
-
-    let randomLeft;
-    do {
-      randomLeft = Math.floor(Math.random() * photoListLeft.length);
-    } while (displayedPhotosLeft.includes(randomLeft));
-
-    displayedPhotosLeft.push(randomLeft);
-
-    document.getElementById("imageOnLeft").src = photoListLeft[randomLeft];
-
-    let randomRight;
-    do {
-      randomRight = Math.floor(Math.random() * photoListRight.length);
-    } while (displayedPhotosRight.includes(randomRight));
-             displayedPhotosRight.push(randomRight);
-     
-    document.getElementById("imageOnRight").src = photoListRight[randomRight];
-
-    if (displayedPhotosLeft.length === photoListLeft.length - 1 && displayedPhotosRight.length === photoListRight.length - 1) {
-      alert("All pictures have been played."); // HERE WROTE THE EVENT TO DISPLAY THE SCORES // CAREFUL +-1
-      alert(scoresString);
+  leftImg.onclick = function() {
+    if (points[images[randomIndex1].country]) {
+      points[images[randomIndex1].country] += 1;
+    } else {
+      points[images[randomIndex1].country] = 1;
     }
+    alert(`You earned a point for ${images[randomIndex1].country}!`);
+    trash.push(randomIndex1);
+    trash.push(randomIndex2);
+    generateRandomImages();
+  }
 
-  });
-  
-  // PHOTOS RIGHT
-
-  imageRight.addEventListener("click", () => {
-    let randomRight;
-    do {
-      randomRight = Math.floor(Math.random() * photoListRight.length);
-    } while (displayedPhotosRight.includes(randomRight));
-  
-    displayedPhotosRight.push(randomRight);
-  
-    document.getElementById("imageOnRight").src = photoListRight[randomRight];
-
-    let randomLeft;
-    do {
-      randomLeft = Math.floor(Math.random() * photoListLeft.length);
-    } while (displayedPhotosLeft.includes(randomLeft));
-             displayedPhotosLeft.push(randomLeft);
-  
-    document.getElementById("imageOnLeft").src = photoListLeft[randomLeft];
-
-    if (displayedPhotosLeft.length === photoListLeft.length - 1 && displayedPhotosRight.length === photoListRight.length - 1) {
-      alert("All pictures have been played."); // HERE WROTE THE EVENT TO DISPLAY THE SCORES // CAREFUL +-1
-      alert(scoresString);
-    } 
-  
-  });
-  
+  rightImg.onclick = function() {
+    if (points[images[randomIndex2].country]) {
+      points[images[randomIndex2].country] += 1;
+    } else {
+      points[images[randomIndex2].country] = 1;
+    }
+    alert(`You earned a point for ${images[randomIndex2].country}!`);
+    trash.push(randomIndex1);
+    trash.push(randomIndex2);
+    generateRandomImages();
+  }
 };
 
-window.onload = function() {
-  generateImages();
-};
+generateRandomImages();
 
-changePicturesLeftAndRight();
-
-selectedPhotos();
-
-
-/////////////////////////////////////////////////////// LANSCAPE PHOTOS /////////////////////////////////////////////////////////////
-
-let winnerNewzealand = ""
-let winnerItaly = ""
-let winnerHawaii = ""
-let winnerNepal = ""
-let winnerBolivia = ""
-let winnerUtah = ""
-
-/////////////////////////////////////////////// TRASH ////////////////////////////////////////////////
-
-// alert(`France: ${points["France"]}\nItaly: ${points["Italy"]}\nJapan: ${points["Japan"]}\nUSA: ${points["USA"]}`);
-
-
-
-// const photoToCountry = {
-//   "France": ["photo1.jpg", "photo2.jpg"],
-//   "Italy": ["photo3.jpg", "photo4.jpg"],
-//   "Japan": ["photo5.jpg", "photo6.jpg"],
-//   "USA": ["photo7.jpg", "photo8.jpg"]
-//   // ...
-// };
-
-// const points = {
-//   "France": 0,
-//   "Italy": 0,
-//   "Japan": 0,
-//   "USA": 0
-//   // ...
-// };
-
-// const displayedPhotos = [];
-
-// const imageLeft = document.getElementById("imageLeft");
-
-// imageLeft.addEventListener("click", () => {
-//   let randomLeft = Math.floor(Math.random() * photoListLeft.length);
-//   let photoName = photoListLeft[randomLeft];
-//   if (!displayedPhotos.includes(randomLeft)) {
-//     displayedPhotos.push(randomLeft);
-
-//     let photoName = 
-
-//     let country = Object.keys(photoList).find(key => photoList[key].includes(photoName));
-//     scores[country] += 1;
-//     alert(`You earned a point for ${country}!`);
-    
-//     if (displayedPhotos.length === photoListLeft.length) {
-//       let scoreString = "Scores:\n";
-//       for (let country in points) {
-//         scoreString += `${country}: ${points[country]}\n`;
-//       }
-//       alert(scoreString);
-//     }
-//   } else {
-//     alert("already played");
-//     return;
-//   }
-// });
-
-
-// const targetValue = selectedPhotos;
-
-// for (const key in photoList) {
-//   if (photoList.hasOwnProperty(key)) {
-//     const array = photoList[key];
-//     if (array.includes(targetValue)) {
-//       console.log(`Found '${targetValue}' in '${key}'`);
-//     }
-//   }
-// }
-
-
-
-    // selectedPhotos.push(document.getElementById("imageOnLeft").src); 
-    // selectedPhotos.push(document.getElementById("imageOnRight").src);
-    // alert(`Selected photos: ${selectedPhotos}`); 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Once all images have been played, display points for each country in an alert
+window.addEventListener('load', function() {
+  if (trash.length === images.length) {
+    let message = "Points:\n";
+    for (let country in points) {
+      message += country + ": " + points[country] + "\n";
+    }
+    alert(message);
+  }
+});
---
+++ trash/script last.js
@@ -0,0 +1,338 @@
+/////////////////////////////////////////////////////// MODAL WINDOW OPENING /////////////////////////////////////////////////////////////
+
+window.addEventListener("load", function(){ // OPEN
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+      },
+      500
+  )
+});
+
+  document.querySelector("#close").addEventListener("click", function(){ //CLOSE 
+  document.querySelector(".popup").style.display = "none";
+  document.querySelector(".backdrop").style.display = "none";
+});
+
+/////////////////////////////////////////////////////// SCORE VARIABLES /////////////////////////////////////////////////////////////
+
+var scores = {
+  "Australia": 0,
+  "Bolivia": 0,
+  "Cuba": 0,
+  "Hawaii": 0,
+  "Iceland": 0,
+  "India": 0,
+  "Italy": 0,
+  "Japan": 0,
+  "Kenya": 0,
+  "Nepal": 0,
+  "Newzealand": 0,
+  "Utah": 0
+};
+
+/////////////////////////////////////////////////////// TRYING OBJECTS /////////////////////////////////////////////////////////////
+
+var photoList = [
+  { country: "Newzeland", points: 0, matchUrl: ["./newzealand/newzealand1.jpg", "./newzealand/newzealand2.jpg", "./newzealand/newzealand3.jpg", "./newzealand/newzealand4.jpg"] },
+  { country: "Italy", points: 0, matchUrl: ["./italy/italy1.jpg", "./italy/italy2.jpg", "./italy/italy3.jpg", "./italy/italy4.jpg"] },
+  { country: "Hawaii", points: 0, matchUrl: ["./hawaii/hawaii4.jpg", "./hawaii/hawaii4.jpg", "./hawaii/hawaii4.jpg", "./hawaii/hawaii4.jpg"] },
+  { country: "Nepal", points: 0, matchUrl: ["./nepal/nepal1.jpg", "./nepal/nepal2.jpg", "./nepal/nepal3.jpg", "./nepal/nepal4.jpg"] },
+  { country: "Bolivia", points: 0, matchUrl: ["./bolivia/bolivia1.jpg", "./bolivia/bolivia2.jpg", "./bolivia/bolivia3.jpg", "./bolivia/bolivia4.jpg"] },
+  { country: "Utah", points: 0, matchUrl: ["./utah/utah1.jpg", "./utah/utah2.jpg", "./utah/utah3.jpg", "./utah/utah4.jpg"] },
+  { country: "Australia", points: 0, matchUrl: ["./australia/australia1.jpg", "./australia/australia2.jpg", "./australia/australia3.jpg", "./australia/australia4.jpg"] },
+  { country: "Cuba", points: 0, matchUrl: ["./cuba/cuba1.jpg", "./cuba/cuba2.jpg", "./cuba/cuba3.jpg", "./cuba/cuba4.jpg"] },
+  { country: "Iceland", points: 0, matchUrl: ["./iceland/iceland1.jpg", "./iceland/iceland2.jpg", "./iceland/iceland3.jpg", "./iceland/iceland4.jpg"] },
+  { country: "India", points: 0, matchUrl: ["./india/india1.jpg", "./india/india2.jpg", "./india/india3.jpg", "./india/india4.jpg"] },
+  { country: "Kenya", points: 0, matchUrl: ["./japan/japan1.jpg", "./japan/japan2.jpg", "./japan/japan3.jpg", "./japan/japan4.jpg"] },
+  { country: "Kenya", points: 0, matchUrl: ["./kenya/kenya1.jpg", "./kenya/kenya2.jpg", "./kenya/kenya3.jpg", "./kenya/kenya4.jpg"] },
+];
+
+/////////////////////////////////////////////////////// ARRAY OF PICTURES LEFTSIDE /////////////////////////////////////////////////////////////
+
+
+
+var photoListLeft = [
+  "./newzealand/newzealand1.jpg",
+  "./newzealand/newzealand2.jpg",
+  "./newzealand/newzealand3.jpg",
+  "./newzealand/newzealand4.jpg",
+  "./italy/italy1.jpg",
+  "./italy/italy2.jpg",
+  "./italy/italy3.jpg",
+  "./italy/italy4.jpg",
+  "./hawaii/hawaii1.jpg",
+  "./hawaii/hawaii2.jpg",
+  "./hawaii/hawaii3.jpg",
+  "./hawaii/hawaii4.jpg",
+  "./nepal/nepal1.jpg",
+  "./nepal/nepal2.jpg",
+  "./nepal/nepal3.jpg",
+  "./nepal/nepal4.jpg",
+  "./bolivia/bolivia1.jpg",
+  "./bolivia/bolivia2.jpg",
+  "./bolivia/bolivia3.jpg",
+  "./bolivia/bolivia4.jpg",
+  "./utah/utah1.jpg",
+  "./utah/utah2.jpg",
+  "./utah/utah3.jpg",
+  "./utah/utah4.jpg"
+]
+
+/////////////////////////////////////////////////////// ARRAY OF PICTURES RIGHTSIDE /////////////////////////////////////////////////////////////
+
+var photoListRight = [
+  "./australia/australia1.jpg",
+  "./australia/australia2.jpg",
+  "./australia/australia3.jpg",
+  "./australia/australia4.jpg",
+  "./cuba/cuba1.jpg",
+  "./cuba/cuba2.jpg",
+  "./cuba/cuba3.jpg",
+  "./cuba/cuba4.jpg",
+  "./iceland/iceland1.jpg",
+  "./iceland/iceland2.jpg",
+  "./iceland/iceland3.jpg",
+  "./iceland/iceland4.jpg",
+  "./india/india1.jpg",
+  "./india/india2.jpg",
+  "./india/india3.jpg",
+  "./india/india4.jpg",
+  "./japan/japan1.jpg",
+  "./japan/japan2.jpg",
+  "./japan/japan3.jpg",
+  "./japan/japan4.jpg",
+  "./kenya/kenya1.jpg",
+  "./kenya/kenya2.jpg",
+  "./kenya/kenya3.jpg",
+  "./kenya/kenya4.jpg"
+]
+
+/////////////////////////////////////////////////////// CHANGING PICTURES /////////////////////////////////////////////////////////////
+
+
+const generateImages = () => {
+  let randomLeft = Math.floor(Math.random() * photoListLeft.length);
+  let randomRight = Math.floor(Math.random() * photoListRight.length);
+
+  document.getElementById("imageOnLeft").src = photoListLeft[randomLeft];
+  document.getElementById("imageOnRight").src = photoListRight[randomRight];
+
+};
+
+
+
+let displayedPhotosLeft = [];
+let displayedPhotosRight = [];
+
+
+const changePicturesLeftAndRight = () => {
+
+let imageLeft = document.getElementById("imageLeft");
+let imageRight = document.getElementById("imageRight");
+
+imageLeft.addEventListener("click", () => {
+
+    let randomLeft;
+    do {
+      randomLeft = Math.floor(Math.random() * photoListLeft.length);
+    } while (displayedPhotosLeft.includes(randomLeft));
+
+    displayedPhotosLeft.push(randomLeft);
+
+    document.getElementById("imageOnLeft").src = photoListLeft[randomLeft];
+
+    let randomRight;
+    do {
+      randomRight = Math.floor(Math.random() * photoListRight.length);
+    } while (displayedPhotosRight.includes(randomRight));
+             displayedPhotosRight.push(randomRight);
+     
+    document.getElementById("imageOnRight").src = photoListRight[randomRight];
+
+    if (displayedPhotosLeft.length === photoListLeft.length - 1 && displayedPhotosRight.length === photoListRight.length - 1) {
+      alert("All pictures have been played."); // HERE WROTE THE EVENT TO DISPLAY THE SCORES // CAREFUL +-1
+      alert(selectedPhotos); 
+    }
+
+  });
+  
+  // PHOTOS RIGHT
+
+  imageRight.addEventListener("click", () => {
+    let randomRight;
+    do {
+      randomRight = Math.floor(Math.random() * photoListRight.length);
+    } while (displayedPhotosRight.includes(randomRight));
+  
+    displayedPhotosRight.push(randomRight);
+  
+    document.getElementById("imageOnRight").src = photoListRight[randomRight];
+
+    let randomLeft;
+    do {
+      randomLeft = Math.floor(Math.random() * photoListLeft.length);
+    } while (displayedPhotosLeft.includes(randomLeft));
+             displayedPhotosLeft.push(randomLeft);
+  
+    document.getElementById("imageOnLeft").src = photoListLeft[randomLeft];
+
+    if (displayedPhotosLeft.length === photoListLeft.length - 1 && displayedPhotosRight.length === photoListRight.length - 1) {
+      alert("All pictures have been played."); // HERE WROTE THE EVENT TO DISPLAY THE SCORES // CAREFUL +-1
+      alert(selectedPhotos);
+    } 
+  
+  });
+  
+};
+
+// Initialiser un tableau vide pour stocker les images sélectionnées
+var selectedImages = [];
+
+// Créer un objet pour stocker les points par pays
+var points = {
+  "newzealand": 0,
+  "italy": 0,
+  "utah": 0,
+  "japan": 0,
+  "bolivia": 0,
+  "australia": 0,
+  "cuba": 0,
+  "kenya": 0,
+  "india": 0,
+  "nepal": 0,
+  "hawaii": 0,
+  "iceland": 0
+};
+
+// Ajouter une fonction pour gérer la sélection de l'image
+function selectImage(event) {
+  // Récupérer l'URL de l'image sélectionnée
+  let imageUrl = event.target.getAttribute("src");
+  
+  // Ajouter l'URL de l'image au tableau selectedImages
+  selectedImages.push(imageUrl);
+  alert(selectedImages);
+  
+  // Si deux images ont été sélectionnées, comparer les URL avec photoList
+  if (selectedImages.length === 2) {
+    // Parcourir photoList et vérifier si les URL correspondent
+    for (let i = 0; i < photoList.length; i++) {
+      if (selectedImages[0] === photoList[i].url && selectedImages[1] === photoList[i].matchUrl) {
+        // Ajouter un point au pays correspondant
+        points[photoList[i].country] += 1;
+        alert(points);
+      }
+    }
+    
+    // Réinitialiser selectedImages pour la prochaine comparaison
+    selectedImages = [];
+  }
+  
+  // Si toutes les images ont été jouées, afficher les points par pays
+  if (Object.keys(points).every(country => points[country] > 0)) {
+    alert("Points:\nFrance: " + points["france"] + "\nItaly: " + points["italy"] + "\nSpain: " + points["spain"] + "\nGermany: " + points["germany"] + "\nUK: " + points["uk"]);
+  }
+};
+
+
+
+window.onload = function() {
+  generateImages();
+};
+
+changePicturesLeftAndRight();
+
+/////////////////////////////////////////////// TRASH ////////////////////////////////////////////////
+
+// alert(`France: ${points["France"]}\nItaly: ${points["Italy"]}\nJapan: ${points["Japan"]}\nUSA: ${points["USA"]}`);
+
+
+
+// const photoToCountry = {
+//   "France": ["photo1.jpg", "photo2.jpg"],
+//   "Italy": ["photo3.jpg", "photo4.jpg"],
+//   "Japan": ["photo5.jpg", "photo6.jpg"],
+//   "USA": ["photo7.jpg", "photo8.jpg"]
+//   // ...
+// };
+
+// const points = {
+//   "France": 0,
+//   "Italy": 0,
+//   "Japan": 0,
+//   "USA": 0
+//   // ...
+// };
+
+// const displayedPhotos = [];
+
+// const imageLeft = document.getElementById("imageLeft");
+
+// imageLeft.addEventListener("click", () => {
+//   let randomLeft = Math.floor(Math.random() * photoListLeft.length);
+//   let photoName = photoListLeft[randomLeft];
+//   if (!displayedPhotos.includes(randomLeft)) {
+//     displayedPhotos.push(randomLeft);
+
+//     let photoName = 
+
+//     let country = Object.keys(photoList).find(key => photoList[key].includes(photoName));
+//     scores[country] += 1;
+//     alert(`You earned a point for ${country}!`);
+    
+//     if (displayedPhotos.length === photoListLeft.length) {
+//       let scoreString = "Scores:\n";
+//       for (let country in points) {
+//         scoreString += `${country}: ${points[country]}\n`;
+//       }
+//       alert(scoreString);
+//     }
+//   } else {
+//     alert("already played");
+//     return;
+//   }
+// });
+
+
+// const targetValue = selectedPhotos;
+
+// for (const key in photoList) {
+//   if (photoList.hasOwnProperty(key)) {
+//     const array = photoList[key];
+//     if (array.includes(targetValue)) {
+//       console.log(`Found '${targetValue}' in '${key}'`);
+//     }
+//   }
+// }
+
+
+
+    // selectedPhotos.push(document.getElementById("imageOnLeft").src); 
+    // selectedPhotos.push(document.getElementById("imageOnRight").src);
+    // alert(`Selected photos: ${selectedPhotos}`); 
+
+
+// function selectedPhotos() {
+
+//   let selectedPhotos = [];
+  
+//   let selectedPhotoLeft = photoListLeft[randomLeft]; 
+//   selectedPhotos.push(selectedPhotoLeft);
+//   alert(`Selected photos: ${selectedPhotos}`);
+
+//   let selectedPhotoRight = photoListRight[randomRight]; 
+//   selectedPhotos.push(selectedPhotoRight);
+//   alert(`Selected photos: ${selectedPhotos}`);
+
+// }
+
+
+
+
+
+
+
+
+
+
